6c93e30f835b951dd2621799db271ebecf7c7147
Malcolm Law <23007687+Meowcolm024@users.noreply.github.com>
2026-06-01 09:36:55 +0800

    Add flake.nix for Nix development environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{  description = "DevShell for ReConf: an experimental configuration language with refinement types";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            iconv
+            pkg-config
+          ];
+        };
+      }
+    );
+}
